4bbf812c5452e6b1579b254e943b057c22fb130d
TUSHAR-hase <thakortushar81315@gmail.com>
2025-08-04 13:29:54 +0530

    fix: removed Vite from Next.js project

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "react/no-unescaped-entities": "off",         // single quotes (') ke warning band
     "react-hooks/exhaustive-deps": "off",         // useEffect dependency warnings band
     "@next/next/no-img-element": "off"     ,
-    "react/react-in-jsx-scope": "off",       // <img> tag ke warning band
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",        // <img> tag ke warning band
   }
 };
